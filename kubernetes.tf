1ef6ee08d8fc48310c60e1b1ca5c8f5254e1eb3b
tlsaudwl <tlsaudwl1821@naver.com>
2024-07-04 10:03:59 +0900

    tf 파일 변경

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,54 +1,54 @@
 provider "kubernetes" {
-  host                   = aws_eks_cluster.hanalink_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.hanalink_cluster.certificate_authority.0.data)
+  host = aws_eks_cluster.new_hanalink_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.new_hanalink_cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.hanalink_cluster.name]
+    command = "aws"
+    args = ["eks", "get-token", "--cluster-name", aws_eks_cluster.new_hanalink_cluster.name, "--region", "ap-northeast-2"]
   }
 }
 
-data "aws_eks_cluster_auth" "hanalink_cluster" {
-  name = aws_eks_cluster.hanalink_cluster.name
+data "aws_eks_cluster_auth" "new_hanalink_cluster" {
+  name = aws_eks_cluster.new_hanalink_cluster.name
 }
 
-resource "kubernetes_deployment" "hanalink_spring_boot_app" {
+resource "kubernetes_deployment" "new_hanalink_spring_boot_app" {
   metadata {
-    name = "hanalink-spring-boot-app"
+    name = "new-hanalink-spring-boot-app"
     labels = {
-      app = "hanalink-spring-boot-app"
+      app = "new-hanalink-spring-boot-app"
     }
   }
   spec {
-    replicas = 2
+    replicas = 1
     selector {
       match_labels = {
-        app = "hanalink-spring-boot-app"
+        app = "new-hanalink-spring-boot-app"
       }
     }
     template {
       metadata {
         labels = {
-          app = "hanalink-spring-boot-app"
+          app = "new-hanalink-spring-boot-app"
         }
       }
       spec {
         container {
-          name  = "hanalink-spring-boot-app"
+          name = "new-hanalink-spring-boot-app"
           image = "tlsaudwl/hanalink-spring-boot:latest"
           port {
             container_port = 8080
           }
           env {
-            name  = "SPRING_DATASOURCE_URL"
+            name = "SPRING_DATASOURCE_URL"
             value = "jdbc:mysql://hanalink-db.cbmoo6w6k67n.ap-northeast-2.rds.amazonaws.com/hanalink"
           }
           env {
-            name  = "SPRING_DATASOURCE_USERNAME"
+            name = "SPRING_DATASOURCE_USERNAME"
             value = "root"
           }
           env {
-            name  = "SPRING_DATASOURCE_PASSWORD"
+            name = "SPRING_DATASOURCE_PASSWORD"
             value = "hanalink123"
           }
         }
@@ -57,45 +57,45 @@ resource "kubernetes_deployment" "hanalink_spring_boot_app" {
   }
 }
 
-resource "kubernetes_service" "hanalink_spring_boot_service" {
+resource "kubernetes_service" "new_hanalink_spring_boot_service" {
   metadata {
-    name = "hanalink-spring-boot-service"
+    name = "new-hanalink-spring-boot-service"
   }
   spec {
     selector = {
-      app = "hanalink-spring-boot-app"
+      app = "new-hanalink-spring-boot-app"
     }
     port {
-      port        = 80
+      port = 80
       target_port = 8080
     }
     type = "LoadBalancer"
   }
 }
 
-resource "kubernetes_deployment" "hanalink_react_app" {
+resource "kubernetes_deployment" "new_hanalink_react_app" {
   metadata {
-    name = "hanalink-react-app"
+    name = "new-hanalink-react-app"
     labels = {
-      app = "hanalink-react-app"
+      app = "new-hanalink-react-app"
     }
   }
   spec {
-    replicas = 2
+    replicas = 1
     selector {
       match_labels = {
-        app = "hanalink-react-app"
+        app = "new-hanalink-react-app"
       }
     }
     template {
       metadata {
         labels = {
-          app = "hanalink-react-app"
+          app = "new-hanalink-react-app"
         }
       }
       spec {
         container {
-          name  = "hanalink-react-app"
+          name = "new-hanalink-react-app"
           image = "tlsaudwl/hanalink-react:latest"
           port {
             container_port = 80
@@ -106,16 +106,16 @@ resource "kubernetes_deployment" "hanalink_react_app" {
   }
 }
 
-resource "kubernetes_service" "hanalink_react_service" {
+resource "kubernetes_service" "new_hanalink_react_service" {
   metadata {
-    name = "hanalink-react-service"
+    name = "new-hanalink-react-service"
   }
   spec {
     selector = {
-      app = "hanalink-react-app"
+      app = "new-hanalink-react-app"
     }
     port {
-      port        = 80
+      port = 80
       target_port = 80
     }
     type = "LoadBalancer"
--- main.tf
+++ main.tf
@@ -2,48 +2,54 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "new_vpc" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "new-vpc"
+  }
 }
 
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+resource "aws_subnet" "new_public" {
+  count = 2
+  vpc_id = aws_vpc.new_vpc.id
+  cidr_block = cidrsubnet(aws_vpc.new_vpc.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "new-public-subnet-${count.index}"
+  }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "new_gw" {
+  vpc_id = aws_vpc.new_vpc.id
+  tags = {
+    Name = "new-igw"
+  }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
+resource "aws_route_table" "new_public" {
+  vpc_id = aws_vpc.new_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.new_gw.id
+  }
+  tags = {
+    Name = "new-public-rt"
   }
 }
 
-resource "aws_route_table_association" "a" {
-  count          = 2
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "new_public" {
+  count = 2
+  subnet_id = aws_subnet.new_public[count.index].id
+  route_table_id = aws_route_table.new_public.id
 }
 
-resource "aws_eks_cluster" "hanalink_cluster" {
-  name     = "hanalink-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.public.*.id
-  }
-}
-
-resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
-
+resource "aws_iam_role" "new_eks_role" {
+  name = "new-eks-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -56,33 +62,32 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "new_eks_policy_attachment" {
+  role = aws_iam_role.new_eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.hanalink_cluster.name
-  node_group_name = "standard-workers"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = aws_subnet.public.*.id
+resource "aws_iam_role_policy_attachment" "new_eks_vpc_resource_controller_policy" {
+  role = aws_iam_role.new_eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
 
-  scaling_config {
-    desired_size = 3
-    max_size     = 5
-    min_size     = 1
+resource "aws_eks_cluster" "new_hanalink_cluster" {
+  name = "new-hanalink-cluster"
+  role_arn = aws_iam_role.new_eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.new_public.*.id
   }
 
-  instance_types = ["t3.medium"]
-
   depends_on = [
-    aws_eks_cluster.hanalink_cluster
+    aws_iam_role_policy_attachment.new_eks_policy_attachment,
+    aws_iam_role_policy_attachment.new_eks_vpc_resource_controller_policy,
   ]
 }
 
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
-
+resource "aws_iam_role" "new_node_role" {
+  name = "new-eks-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -95,17 +100,39 @@ resource "aws_iam_role" "node_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy_attachment" {
-  role       = aws_iam_role.node_role.name
+resource "aws_iam_role_policy_attachment" "new_node_policy_attachment" {
+  role = aws_iam_role.new_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy_attachment_ecr" {
-  role       = aws_iam_role.node_role.name
+resource "aws_iam_role_policy_attachment" "new_node_policy_attachment_ecr" {
+  role = aws_iam_role.new_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy_attachment_cni" {
-  role       = aws_iam_role.node_role.name
+resource "aws_iam_role_policy_attachment" "new_node_policy_attachment_cni" {
+  role = aws_iam_role.new_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_node_group" "new_node_group" {
+  cluster_name = aws_eks_cluster.new_hanalink_cluster.name
+  node_group_name = "new-standard-workers"
+  node_role_arn = aws_iam_role.new_node_role.arn
+  subnet_ids = aws_subnet.new_public.*.id
+
+  scaling_config {
+    desired_size = 3
+    max_size = 5
+    min_size = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_eks_cluster.new_hanalink_cluster,
+    aws_iam_role_policy_attachment.new_node_policy_attachment,
+    aws_iam_role_policy_attachment.new_node_policy_attachment_ecr,
+    aws_iam_role_policy_attachment.new_node_policy_attachment_cni,
+  ]
 }
